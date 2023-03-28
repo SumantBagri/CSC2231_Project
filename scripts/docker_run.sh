6b651e6677c866f08a35962b9b7589c275d1ec96
SumantBagri <sbagri.sumant@gmail.com>
2023-03-28 19:57:25 -0400

    Remove port from docker run and add tensorrt python wheel

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker run --runtime nvidia -it -p 8888:8888 -v ~/project:/opt/project --name ldm -d trapdoor20/csc2231:$1
+docker run --runtime nvidia -it --network host -v ~/project:/opt/project --name ldm -d trapdoor20/csc2231:$1
 docker logs ldm --since 3s
