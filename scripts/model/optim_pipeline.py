ff439266b45b4f5f5084d39abc90b16cf693e9cf
juggernaut <sbagri.sumant@gmail.com>
2023-04-09 15:46:27 +0800

    Evaluation datafiles updated (RTX 3070)

---
+++ scripts/model/optim_pipeline.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+import torch
+import torch.onnx
+
+from ldm_uncond.latent_diffusion_uncond import LDMPipeline
+
+if __name__ == '__main__':
+    diffusion_pipeline = LDMPipeline()
+
+    DTYPE = torch.float16
+    DEVICE = torch.device('cuda')
+
+    diffusion_pipeline = diffusion_pipeline.to(device=DEVICE, dtype=DTYPE)
+
+    if len(sys.argv) > 1:
+        diffusion_pipeline.export_pipeline_to_onnx(fname=sys.argv[1])
+    else:
+        diffusion_pipeline.export_pipeline_to_onnx()
